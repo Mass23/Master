e08ec5fa50938364ba0b2d4269b0628697f5bec5
Mass23 <33321080+Mass23@users.noreply.github.com>
2018-08-07 09:45:11 +0200

    Create alignment.sh

---
+++ alignment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# initial example of a pipeline script
+
+bwa index reference.fa
+bwa aln -I -t 8 reference.fa s_1.txt > out.sai
+bwa samse reference.fa out.sai s_1.txt > out.sam
+
+samtools view -bSu out.sam  | samtools sort -  out.sorted
