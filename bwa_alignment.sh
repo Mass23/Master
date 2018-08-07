e56d3ab960f853085d8957e5b12a3bd97396f77e
Mass23 <33321080+Mass23@users.noreply.github.com>
2018-08-07 11:00:35 +0200

    Update bwa_alignment.sh

--- bwa_alignment.sh
+++ bwa_alignment.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#
-# initial example of a pipeline script
 
-bwa aln -I -t 8 reference.fa s_1.txt > out.sai
+bwa mem reference.fa s_1.txt > out.sai
 bwa samse reference.fa out.sai s_1.txt > out.sam
 
 samtools view -bSu out.sam  | samtools sort -  out.sorted
