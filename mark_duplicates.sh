b05d6caa50b3a0b9c490bcbac77d08c400ab1b40
Mass23 <33321080+Mass23@users.noreply.github.com>
2018-08-14 18:27:02 +0200

    Update mark_duplicates.sh

--- mark_duplicates.sh
+++ mark_duplicates.sh
@@ -1,3 +1,10 @@
+# SAM to BAM
+java -jar picard.jar SortSam \ 
+    INPUT=aligned_reads.sam \ 
+    OUTPUT=sorted_reads.bam \ 
+    SORT_ORDER=coordinate 
+
+# Mark duplicates
 java -Xmx1g -jar /apps/PICARD/1.95/MarkDuplicates.jar \
                             MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=1000\
                             METRICS_FILE=out.metrics \
