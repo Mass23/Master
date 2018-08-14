39f685c4c692dc75d3a3ef93ebe09b326bcbe322
Mass23 <33321080+Mass23@users.noreply.github.com>
2018-08-14 18:27:54 +0200

    Update bwa_alignment.sh

--- bwa_alignment.sh
+++ bwa_alignment.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-bwa mem reference.fa s_1.txt > out.sai
-bwa samse reference.fa out.sai s_1.txt > out.sam
-
-samtools view -bSu out.sam  | samtools sort -  out.sorted
+bwa mem -M reference.fa s_1.txt > out.sam
