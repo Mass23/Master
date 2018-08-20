074e7f7cc3d6a45cc77518a5ea3153ecb3ee1aa9
Mass23 <33321080+Mass23@users.noreply.github.com>
2018-08-20 17:16:19 +0200

    Update 1.2_reads_trimming.py

--- 1.2_reads_trimming.py
+++ 1.2_reads_trimming.py
@@ -16,30 +16,31 @@ def trimmomatic_paired(leading, trailing, slidingwindow1, slidingwindow2, minlen
     for fastqfile in fastqlist:
         print(fastqfile, " in process...")
 
-        if "_R1_" in fastqfile:
+        if "_R1" in fastqfile:
             r1dir = fastqfile
-            r2dir = r1dir.replace("R1", "R2")
+            r2dir = r1dir.replace("_R1", "_R2")
                     
-        elif "_R2_" in fastqfile:
+        elif "_R2" in fastqfile:
             r2dir = fastqfile
-            r1dir = r2dir.replace("R2", "R1")
+            r1dir = r2dir.replace("_R2", "_R1")
 
         no_paired = r1dir.replace("_R1", "")
 
         args = ["trimmomatic", "PE", "-phred33",
         # Input R1, R2
-        r1dir, r2dir,
-        # Output
-        "/trimmed_reads/" + no_paired.replace(".fastq.gz", "") + "_forward_paired.fq.gz",
-        "/trimmed_reads/" + no_paired.replace(".fastq.gz", "") + "_forward_unpaired.fq.gz",
-        "/trimmed_reads/" + no_paired.replace(".fastq.gz", "") + "_reverse_paired.fq.gz",
-        "/trimmed_reads/" + no_paired.replace(".fastq.gz", "") + "_reverse_unpaired.fq.gz",
+        "/" + r1dir, "/" + r2dir,
+        # Output forward/reverse, paired/unpaired
+        "/trimmed_reads/" + no_paired.replace(".fastq.gz", "") + "_forward_paired.fastq.gz",
+        "/trimmed_reads/" + no_paired.replace(".fastq.gz", "") + "_forward_unpaired.fastq.gz",
+        "/trimmed_reads/" + no_paired.replace(".fastq.gz", "") + "_reverse_paired.fastq.gz",
+        "/trimmed_reads/" + no_paired.replace(".fastq.gz", "") + "_reverse_unpaired.fastq.gz",
+        "ILLUMINACLIP:TruSeq3-PE.fa:2:30:10",
         "LEADING:" + str(leading),
         "TRAILING:" + str(trailing),
         "SLIDINGWINDOW:" + str(slidingwindow1) + ":" + str(slidingwindow2),
         "MINLEN:" + str(minlen)]
 
-        subprocess.call(" ".join(args))
+        subprocess.run(" ".join(args))
 
         fastqlist.remove(r1dir)
         fastqlist.remove(r2dir)
