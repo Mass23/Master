b5542a99aa5e83b2a70bd33387b60b6c7e533f14
Mass23 <33321080+Mass23@users.noreply.github.com>
2018-08-22 18:11:27 +0200

    Update 1.4_bwa_alignment.sh

--- 1.4_bwa_alignment.sh
+++ 1.4_bwa_alignment.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 import subprocess
+import os
 
-file_list = [i for i in glob.glob(os.getcwd()) if i.endswith(".fq.gz")]
+files = [f for f in os.listdir(os.getcwd()) if f.endswith("paired.fq.gz")]
 
-subprocess.call("bwa mem " + "fsel*.fasta " + " ".join(mlist) + " > alignment.sam")
+subprocess.call("bwa mem " + "fsel*.fasta " + " ".join(files) + " > alignment.sam", shell = True)
