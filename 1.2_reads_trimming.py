c1e5b90a6ff8745fe3f233e8730930aab8fba56a
Mass23 <33321080+Mass23@users.noreply.github.com>
2018-10-02 03:42:45 +0200

    Update 1.2_reads_trimming.py

--- 1.2_reads_trimming.py
+++ 1.2_reads_trimming.py
@@ -1,9 +1,9 @@
 # A script that processes alle the fastq files in a directory with trimmomatic.
 
-# LEADING = removes the leading bases under the threshold
-# TRAILING = removes the trailing bases under the threshold
+# LEADING = removes the leading bases under the threshold
+# TRAILING = removes the trailing bases under the threshold
 # MINLEN = drops the reads shorter than the threshold
-# SLIDINGWINDOW = removes the k-mers under a quality threshold
+# SLIDINGWINDOW = removes the k-mers under a quality threshold
 import os
 
 def trimmomatic_paired(leading, trailing, slidingwindow1, slidingwindow2, minlen):
@@ -11,13 +11,13 @@ def trimmomatic_paired(leading, trailing, slidingwindow1, slidingwindow2, minlen
     import glob
 
     fastqlist = glob.glob("*.fastq.gz")
-        
+
     for fastqfile in fastqlist:
 
         if "_R1" in fastqfile:
             r1dir = fastqfile
             r2dir = r1dir.replace("_R1", "_R2")
-                    
+
         elif "_R2" in fastqfile:
             r2dir = fastqfile
             r1dir = r2dir.replace("_R2", "_R1")
@@ -28,19 +28,19 @@ def trimmomatic_paired(leading, trailing, slidingwindow1, slidingwindow2, minlen
 
         no_paired = r1id.replace("_R1", "")
 
-        args = ["trimmomatic", "PE", "-threads 2", "-phred33",
-        # Input R1, R2
-        r1id + ".fastaq.gz", r2id + ".fastaq.gz",
-        # Output forward/reverse, paired/unpaired
-        no_paired + "_forward_paired.fq.gz",
-        no_paired + "_forward_unpaired.fq.gz",
-        no_paired + "_reverse_paired.fq.gz",
-        no_paired + "_reverse_unpaired.fq.gz",
-        "ILLUMINACLIP:TruSeq3-PE.fa:2:30:10",
-        "LEADING:" + str(leading),
-        "TRAILING:" + str(trailing),
-        "SLIDINGWINDOW:" + str(slidingwindow1) + ":" + str(slidingwindow2),
-        "MINLEN:" + str(minlen)]
+        args = ["trimmomatic", "PE", "-threads 8", "-phred33",
+                # Input R1, R2
+                r1id + ".fastq.gz", r2id + ".fastq.gz",
+                # Output forward/reverse, paired/unpaired
+                no_paired + "_forward_paired.fq.gz",
+                no_paired + "_forward_unpaired.fq.gz",
+                no_paired + "_reverse_paired.fq.gz",
+                no_paired + "_reverse_unpaired.fq.gz",
+                "ILLUMINACLIP:TruSeq3-PE.fa:2:30:10",
+                "LEADING:" + str(leading),
+                "TRAILING:" + str(trailing),
+                "SLIDINGWINDOW:" + str(slidingwindow1) + ":" + str(slidingwindow2),
+                "MINLEN:" + str(minlen)]
 
         subprocess.call(" ".join(args), shell = True)
         print(no_paired, " done!")
