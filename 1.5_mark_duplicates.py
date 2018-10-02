ab5ec7158a621cd25e3cce45682930649a2a00e4
Mass23 <33321080+Mass23@users.noreply.github.com>
2018-10-02 03:31:39 +0200

    Update 1.5_mark_duplicates.py

--- 1.5_mark_duplicates.py
+++ 1.5_mark_duplicates.py
@@ -2,16 +2,17 @@ import glob
 import os
 import subprocess
 
-curr_dir = os.getcwd()
+list_files = glob.glob('*_alignment.sam')
+individuals = [i.split("_")[0] for i in list_files]
+print(individuals)
 
-individuals = [file.split('.')[0] for file in glob.glob(curr_dir) if file.endswith('_aln.sam')]
-
+# Mark duplicates
 for ind in individuals:
-    args_sam2bam = ['java', '-jar', 'picard.jar', 'SortSam', 'INPUT=' + ind + '_aln.sam', 'OUTPUT=' + ind + '_aln_sorted.bam', 'SORT_ORDER=coordinate']
-    subprocess.call(' '.join(args_sam2bam))
+    args_sam2bam = ['picard-tools', 'SortSam', 'INPUT=' + ind + '_alignment.sam', 'OUTPUT=' + ind + '_aln_sorted.bam', 'SORT_ORDER=coordinate']
+    subprocess.call(' '.join(args_sam2bam), shell = True)
 
-    args_dedup = ['java', '-jar', 'picard.jar', 'MarkDuplicates', 'I=' + ind + '_aln_sorted.bam', 'O=' + ind + '_dedup.bam', 'M=' + ind + '_dedup_metrics.txt']
-    subprocess.call(' '.join(args_dedup))
+    args_dedup = ['picard-tools', 'MarkDuplicates', 'I=' + ind + '_aln_sorted.bam', 'O=' + ind + '_dedup.bam', 'M=' + ind + '_dedup_metrics.txt']
+    subprocess.call(' '.join(args_dedup), shell = True)
 
-    args_index = ['java', '-jar', 'picard.jar', 'BuildBamIndex', 'INPUT=' + ind + 'dedup_reads.bam']
-    subprocess.call(' '.join(args_index))
+    args_index = ['picard-tools', 'BuildBamIndex', 'INPUT=' + ind + '_dedup_reads.bam']
+    subprocess.call(' '.join(args_index), shell = True)
