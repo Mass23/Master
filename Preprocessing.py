4daee7b3e437f7e8c60087e88b16d129798d72f6
Mass23 <33321080+Mass23@users.noreply.github.com>
2018-10-16 15:41:49 +0200

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -2,8 +2,28 @@
 import subprocess
 import glob
 import multiprocessing
+import sys
 
-def trimmomatic(fastqfile, individual):
+# Arguments: python3 preprocessing.py individual reference core_nb
+args = sys.argv
+individual = args[1]
+ref = args[2]
+cores = str(args[3])
+
+print("Individual: ", individual)
+print("Reference: ", ref)
+
+# Create needed indexes
+subprocess.call('bwa index ' + ref, shell = True)
+subprocess.call('picard-tools CreateSequenceDictionary MAX_RECORDS_IN_RAM=null R=' + ref + ' O=' + ref.split('.')[0] + '.dict', shell = True)
+subprocess.call('samtools faidx ' + ref, shell = True)
+
+fastq_list = glob.glob(individual + '*.fastq.gz')
+fastqfile = fastq_list[0]
+
+def trimmomatic(fastqfile, individual, cores):
+    r1 = ''
+    r2 = ''
 
     if "_R1" in fastqfile:
         r1 = fastqfile
@@ -13,7 +33,7 @@ def trimmomatic(fastqfile, individual):
         r2 = fastqfile
         r1 = r2.replace("_R2", "_R1")
 
-    args = ["trimmomatic", "PE", "-threads 8", "-phred33",
+    args = ["trimmomatic", "PE", "-threads",  cores, "-phred33",
             # Input R1, R2
             r1 , r2,
             # Output forward/reverse, paired/unpaired
@@ -22,25 +42,27 @@ def trimmomatic(fastqfile, individual):
             individual + "_reverse_paired.fq.gz",
             individual + "_reverse_unpaired.fq.gz",
             "ILLUMINACLIP:TruSeq3-PE.fa:2:30:10",
-            "LEADING: 3",
-            "TRAILING: 3",
+            "LEADING:3",
+            "TRAILING:3",
             "SLIDINGWINDOW:4:15",
             "MINLEN:36"]
 
     subprocess.call(' '.join(args), shell = True)
 
-def bwa_map(individual, ref):
+
+def bwa_map(individual, ref, cores):
 
     # Merge forward and reverse files
-    args_forward = ["cat", individual + "*forward_paired.fq.gz", ">", individual + "_forward.fastq.gz"]
-    args_reverse = ["cat", individual + "*reverse_paired.fq.gz", ">", individual + "_reverse.fastq.gz"]
+    args_forward = ["cat", individual + "_forward_paired.fq.gz", ">", individual + "_forward.fastq.gz"]
+    args_reverse = ["cat", individual + "_reverse_paired.fq.gz", ">", individual + "_reverse.fastq.gz"]
 
     subprocess.call(" ".join(args_forward), shell = True)
     subprocess.call(" ".join(args_reverse), shell = True)
 
     # Map reads
-    map_args = ["bwa", "mem", "-T", "8", "-M", "-R", "'" + r"@RG\tID:" + individual + r"\t'" ,ref, individual + "_forward.fastq.gz", individual + "_reverse.fastq.gz", ">", individual + "_alignment.sam"]
+    map_args = ["bwa", "mem", "-T", cores, "-M", "-R", "'" + r"@RG\tID:" + individual + r"\tSM:" + individual + r"\t'" ,ref, individual + "_forward.fastq.gz", individual + "_reverse.fastq.gz", ">", individual + "_alignment.sam"]
     subprocess.call(" ".join(map_args), shell = True)
+
 
 def picard_dedup(individual):
 
@@ -56,8 +78,8 @@ def picard_dedup(individual):
     args_index = ['picard-tools', 'BuildBamIndex', 'INPUT=' + individual + '_dedup.bam']
     subprocess.call(' '.join(args_index), shell = True)
 
-def realign_indels(individual, ref):
 
+def realign_indels(individual, ref):
 
     # Index bam
     args_index = ['samtools', 'index', individual + '_dedup.bam']
@@ -71,43 +93,41 @@ def realign_indels(individual, ref):
     realign = ['GenomeAnalysisTK', '-T', 'IndelRealigner', '-R', ref, '-I', individual + '_dedup.bam', '-targetIntervals', individual + 'forIndelRealigner.intervals', '-o', individual + '_realigned.bam']
     subprocess.call(' '.join(realign), shell = True) 
 
-def call_snp(individual, ref):
 
-    # Call snp
-    call_args = ['GenomeAnalysisTK -T HaplotypeCaller -R', ref, '-I', individual + '_realigned.bam' , '-ERC GVCF -o', individual + '.g.vcf']
-    subprocess.call(' '.join(call_args), shell = True)
+def snp_call(individual):
+    args_call = ['gatk', 'HaplotypeCaller', '-R', 'fsel_M.fasta', '-I', individual + '_realigned.bam', '-ERC', 'GVCF', '-o', individual + '.g.vcf']
+    subprocess.call(' '.join(args_call))
 
-################################################################
-# 1. Index genome, list files
-ref = 'fsel_M.fasta'
-subprocess.call('bwa index ' + ref, shell = True)
-subprocess.call('picard-tools CreateSequenceDictionary R=' + ref + ' O=' + ref.split('.')[0] + '.dict', shell = True)
-subprocess.call('samtools faidx ' + ref, shell = True)
 
-fastq_list = glob.glob('*.fastq.gz')
-
-################################################################
-# 2. Preprocess
-def preprocess(fastqfile, ref):
+def preprocess(fastqfile, ref, cores):
 
     individual = fastqfile.split('_')[0]
 
-    # 2.1 trimming
-    trimmomatic(fastqfile, individual)
+    # 2.1 trimming - TO CORRECT INPUT
+    print("Trimming...")
+    trimmomatic(fastqfile, individual, cores)
+    print("Trimming done!")
 
     # 2.2 mapping
-    bwa_map(individual, ref)
+    print("Mapping...")
+    bwa_map(individual, ref, cores)
+    print("Mapping done!")
 
     # 2.3 mark duplicates
+    print("Marking duplicates...")
     picard_dedup(individual)
+    print("Duplicates marked!")
 
-    # 2.4 Realign indels
+    print("Calling variants...")
+    snp_call(individual)
+    print("Variants called!")
+
+    print("Realignind indels...")
     realign_indels(individual, ref)
+    print("Realignment done!")
 
-    # 2.4 call snp
-    call_snp(individual, ref)
+    print("Preprocessing done!")
 
-p = multiprocessing.Pool(64)
-p.map(preprocess, fastq_list)
-p.close()
+preprocess(fastqfile, ref, cores)
+
 
