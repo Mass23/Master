26bfe51a90168620a59c3351e00a665581822851
Mass23 <33321080+Mass23@users.noreply.github.com>
2019-01-08 11:14:42 +0100

    Update fasta2snipre.py

--- fasta2snipre.py
+++ fasta2snipre.py
@@ -1,11 +1,20 @@
-# Structure of the script arguments:
-# python3 snipre.py annotation reference outgroup genome1 genome2 etc.
-import subprocess
-import sys
+# Tested individuals fasta files finish with *1.fa and *2.fa corresponding to the haplotypes
+# Outgroup sequence finishes with *A.fa
 import glob
 from Bio import SeqIO
-import numpy
-import operator
+
+class results:
+    def __init__(self, pr, fr, ps, fs, tsil, trep, sample_size, identity,length,ncount):
+        self.pr = pr
+        self.fr = fr
+        self.ps = ps
+        self.fs = fs
+        self.tsil = tsil
+        self.trep = trep
+        self.sample_size = sample_size
+        self.identity = identity
+        self.length = length
+        self.ncount = ncount
 
 def get_amino_acid(codon):
     """ A function that returns the amino acid corresponding to a codon."""
@@ -95,204 +104,224 @@ def get_amino_acid(codon):
     'TTG' : 'LEU',
     'TTT' : 'PHE'}
 
-    return codon_table[codon]
+    return(codon_table[codon])
 
-def get_nout_npop(reference_genome, individuals_genome):
-    with open(reference_genome) as f:
-        nout = len(SeqIO.parse(f, 'fasta'))
-    with open(individuals_genome) as f:
-        npop = len(SeqIO.parse(f, 'fasta'))
-    return([nout,npop])
+def process_genes(individuals_list, gene_dict, outgroup_dict):
 
-def get_geneset_size(file):
-    gene_count = 0
-    for i in SeqIO.parse(open(file), "fasta"):
-        if len(i.seq)/ 3 == len(i.seq) // 3:
-            gene_count +=1
-    return(gene_count)
+    gene_list = outgroup_dict.keys()
+    results_dict = {}
 
-class gene:
-    def __init__(self, individual, gene_id, gene_sequence):
-        self.individual = individual
-        self.gene_id = gene_id
-        self.gene_sequence = gene_sequence
-class result:
-    def __init__(self, gene_id, pr, ps, fr, fs, tsil, trepl):
-        self.gene_id = gene_id
-        self.pr = pr
-        self.ps = ps
-        self.fr = fr
-        self.fs = fs
-        self.tsil = tsil
-        self.trepl = trepl
+    for gene in gene_list:
 
-##### 1. Parse arguments ####################################################################################################
-print("Parsing ...")
-arg_list = list(sys.argv)
-annotation_file = arg_list[1]
-reference_genome = arg_list[2]
+        gene_length = len(outgroup_dict[gene])
 
-# With the outgroup as first genome of the individuals list (to be on the top of the matrix)
-outgroup_genome = arg_list[3]
-individuals_genome = arg_list[3:]
-tested_individuals = arg_list[4:]
+        for codon in range(0,gene_length,3):
 
-##### 2. Get nout/npop ######################################################################################################
-nout_npop = get_nout_npop(reference_genome, individuals_genome[0])
-nout = nout_npop[0]
-npop = nout_npop[1]
+            pos1 = codon
+            pos2 = codon + 1
+            pos3 = codon + 2
 
-##### 3. Extract transcripts ################################################################################################
-# Subprocess samtools faidx to index the reference genome and run subgffread to extract coding sequences
-print("Indexing genomes, extracting transcripts ...")
-args_faidx = ["samtools", "faidx", reference_genome]
-subprocess.call(" ".join(args_faidx), shell = True)
+            outgroup_codon = outgroup_dict[gene][pos1] + outgroup_dict[gene][pos2] + outgroup_dict[gene][pos3]
+            outgroup_amino_acid = get_amino_acid(outgroup_codon)
 
-for individual in individuals_genome:
+            population_codons = set()
+            population_amino_acids = set()
 
-    args_faidx = ["samtools", "faidx", individual]
-    subprocess.call(" ".join(args_faidx), shell = True)
+            for individual in individuals_list:
+                individual_codon = gene_dict[individual][gene][pos1] + gene_dict[individual][gene][pos2] + gene_dict[individual][gene][pos3]
 
-    args_subgffread = ("gffread", "-C", "-J", "-w", individual + "_transcripts.fa", "-g", individual, annotation_file)
-    subprocess.call(" ".join(args_subgffread), shell = True)
+                if 'N' or '*' or '-' in individual_codon:
+                    continue
+                else:
+                    population_codons.add(individual_codon)
+                    population_amino_acids.add(get_amino_acid(individual_codon))
 
-transcript_files = glob.glob("*_transcripts.fa")
 
-##### 4. Create genes matrix ################################################################################################
-# Create empty matrix with width of the first fasta file length
-data = numpy.empty(shape=[0, get_geneset_size(transcript_files[0])])
 
-# Fill in the matrix with the gene class instances. Each iteration adds an individual on the next row with all the gene class instances on the columns.
-print("Creating gene matrix ...")
-for individual in transcript_files:
+        else:
+            continue
 
-    name = individual.split(".")[0]
-    gene_list = numpy.array(())
+# Create outgroup dictionary
+outgroup = glob.glob('*_transcriptsA.fa')[0]
+outgroup_dict = {}
+
+for index, gene in enumerate(SeqIO.parse(open(outgroup), 'fasta')):
+    if len(str(gene.seq)) // 3 == len(str(gene.seq)) / 3:
+
+        gene_name = str(gene.id.replace('-RA','').replace('-RB','').replace('-RC','').replace('-RD','').replace('-RE','').replace('-RF','').replace('-RG','').replace('-RH','').replace('-RI','').replace('-RJ','').replace('-RK','').replace('-RL',''))
+
+        other_transcripts = [i for i in outgroup_dict.keys() if i.startswith(gene_name)]
+
+        try:
+            last_length = len(outgroup_dict[other_transcripts[0]])
+        except:
+            last_length = 0
+
+        if last_length > len(gene.seq):
+            continue
+        elif last_length == len(gene.seq):
+            continue
+        else:
+
+            for k in list(outgroup_dict.keys()):
+                if k.startswith(gene_name):
+                    outgroup_dict.pop(k)
+
+            outgroup_dict[gene.id] = str(gene.seq)
+    else:
+        continue
+
+# Create individuals dictionaries
+individual_files = list(glob.glob('*_transcripts1.fa')) + list(glob.glob('*_transcripts2.fa'))
+
+gene_dict = {}
+
+for individual in individual_files:
+    individual_name = individual.replace('_transcripts1.fa','_1').replace('_transcripts2.fa','_2')
+
+    for index, gene in enumerate(SeqIO.parse(open(individual), 'fasta')):
+        if len(str(gene.seq)) // 3 == len(str(gene.seq)) / 3:
+            if individual_name in gene_dict.keys():
+                gene_dict[individual_name][gene.id] = str(gene.seq)
+            else:
+                gene_dict[individual_name] = dict()
+                gene_dict[individual_name][gene.id] = str(gene.seq)
+        else:
+            continue
+
+print('Outgroup: ', outgroup.replace('_transcripts1.fa',''), ' - ', len(outgroup_dict.keys()), ' genes')
+for individual in gene_dict.keys():
+    print('  - ', individual, ' - ', len(gene_dict[individual].keys()), ' genes')
+
+# Process genes
+genes_list = list(outgroup_dict.keys())
+
+with open('snipre_input.csv', 'w') as output:
+    nout = 27
+    npop = 27
+
+    output.write('\t'.join(['gene_id','PR','FR','PS','FS','Tsil','Trepl','nout','npop']) + '\n')
+
+    count = 0
+    total = len(genes_list) + 1
+
+    for gene in genes_list:
+        count += 1
+
+        print('Gene: ', gene, ' - ', count, '/', total)
+        outgroup_sequence = outgroup_dict[gene]
+
+        individuals_sequence = []
+
+        for individual in list(gene_dict.keys()):
+            try:
+                individuals_sequence.append(gene_dict[individual][gene])
+            except:
+                continue
+
+        sample_size = len(individuals_sequence)
+        gene_length = len(outgroup_sequence)
+
+        ############################# MKR stats #############################
+
+        dn = 0
+        ds = 0
+        pn = 0
+        ps = 0
+        tsil = 0
+        trepl = 0
+        identity = 0
+        ncount = 0
+
+        # 1. Get codons
+        for i in range(0,gene_length,3):
+
+            pos1 = i
+            pos2 = i+1
+            pos3 = i+2
+
+            outgroup_codon = outgroup_sequence[pos1] + outgroup_sequence[pos2] + outgroup_sequence[pos3]
+
+            if 'N' in outgroup_codon or '-' in outgroup_codon or '*' in outgroup_codon:
+                ncount += 1
+                continue
+            else:
+                outgroup_amino_acid = get_amino_acid(outgroup_codon)
+
+
+            individuals_codons = set()
+            individuals_amino_acids = set()
+
+            for individual in individuals_sequence:
+                try:
+                    current_individual_codon = individual[pos1] + individual[pos2] + individual[pos3]
+                except:
+                    continue
+
+                if 'N' in current_individual_codon or '-' in current_individual_codon or '*' in current_individual_codon:
+                    continue
+                else:
+                    current_individual_amino_acid = get_amino_acid(current_individual_codon)
+                    individuals_codons.add(current_individual_codon)
+                    individuals_amino_acids.add(current_individual_amino_acid)
+
+            # 2. Calculate stats
+
+            # TSIL/TREP
+            nucleotides = ['A', 'T', 'G', 'C']
+
+            pos1, pos2, pos3 = outgroup_codon
+
+            position1 = []
+            position2 = []
+            position3 = []
+
+            for nuc in nucleotides:
+                position1.append(str(get_amino_acid(nuc + pos2 + pos3)))
+                position2.append(str(get_amino_acid(pos1 + nuc + pos3)))
+                position3.append(str(get_amino_acid(pos1 + pos2 + nuc)))
+
+            aa1 = len(position1) - position1.count(str(get_amino_acid(outgroup_codon)))
+            aa2 = len(position2) - position2.count(str(get_amino_acid(outgroup_codon)))
+            aa3 = len(position3) - position3.count(str(get_amino_acid(outgroup_codon)))
+
+            trepl += (aa1/3) + (aa2/3) + (aa3/3)
+            tsil += 3 - ((aa1/3) + (aa2/3) + (aa3/3))
+
+            # Na or No change
+            if len(individuals_codons) == 0:
+                ncount +=1
+            elif len(individuals_codons) == 1 and ''.join(individuals_codons) == outgroup_codon:
+                identity += 1
+
+            # Substitutions
+            elif len(individuals_codons) == 1 and outgroup_codon not in individuals_codons:
+                # Non-synonymous
+                if ''.join(individuals_amino_acids) == outgroup_amino_acid:
+                    ds += 1
+                # Synonymous
+                else:
+                    dn += 1
+            # Polymorphisms
+            elif len(individuals_codons) > 1 :
+                # Non-synonymous
+                if len(individuals_amino_acids) > 1:
+                    pn += 1
+                # Synonymous
+                elif len(individuals_amino_acids) == 1:
+                    ps += 1
+            else:
+                print('PROBLEM:')
+                print('gene: ', gene)
+                print('outgroup:')
+                print(outgroup_codon, outgroup_amino_acid)
+                print('individuals:')
+                print(individuals_codons, individuals_amino_acids)
+
+        try:
+            raw_mkr = (dn / ds) / (pn / ps)
+        except:
+            raw_mkr = 'NA'
+        # Get summary stats
+        output.write('\t'.join([str(gene),str(pn),str(dn),str(ps),str(ds),str(tsil),str(trepl),str(nout),str(npop)]) + '\n')
     
-    with open(individual) as handle:
-        for record in SeqIO.parse(handle, "fasta"):
-            if len(record.seq)/ 3 == len(record.seq) // 3:
-                gene_name = record.id
-                gene_sequence = record.seq
-                individual = name
-                gene_list = numpy.hstack((gene_list, gene(individual, gene_name, gene_sequence)))
-        
-    data = numpy.vstack((data, gene_list))
-
-##### 5. Prepare results output #############################################################################################
-# Create a dictionary for the results, iterate over the columns, compare the sequences and calculate the metrics
-results = {}
-
-rows_number = numpy.shape(data)[0]
-columns_number = numpy.shape(data)[1]
-print("Data matrix created!")
-print(rows_number, " individual(s)")
-print(columns_number, " gene(s)")
-
-##### 6. Analysis, print results #############################################################################################
-def get_dndspnps(matrix, sequence_length):
-    outgroup_sequence = matrix[0]
-    individuals_sequences = matrix[1:]
-
-    pr_count = 0
-    fr_count = 0
-    ps_count = 0
-    fs_count = 0
-    trepl = 0
-    tsil = 0
-
-    for i in range(0, sequence_length, 3):
-        outgroup_codon = outgroup_sequence[i] + outgroup_sequence[i + 1] + outgroup_sequence[i + 2]
-
-        # tsil/trep
-        nucleotides = ['A', 'T', 'G', 'C']
-
-        pos1, pos2, pos3 = outgroup_codon
-
-        position1 = list()
-        position2 = list()
-        position3 = list()
-
-        for nuc in nucleotides:
-            position1.append(str(get_amino_acid(nuc + pos2 + pos3)))
-            position2.append(str(get_amino_acid(pos1 + nuc + pos3)))
-            position3.append(str(get_amino_acid(pos1 + pos2 + nuc)))
-
-        aa1 = len(position1) - position1.count(str(get_amino_acid(outgroup_codon)))
-        aa2 = len(position2) - position2.count(str(get_amino_acid(outgroup_codon)))
-        aa3 = len(position3) - position3.count(str(get_amino_acid(outgroup_codon)))
-
-        trepl += (aa1/3) + (aa2/3) + (aa3/3)
-        tsil += 3 - ((aa1/3) + (aa2/3) + (aa3/3))
-
-        # PR/FR/PS/FS
-        individuals_codons = set()
-        for individual in individuals_sequences:
-            codon =  individual[i] + individual[i + 1] + individual[i + 2]
-            individuals_codons.add(codon)
-        
-        # If No change or substitution
-        if len(individuals_codons) == 1:
-            
-            individual_codon = str(''.join(individuals_codons))
-
-            # No change
-            if individual_codon == outgroup_codon:
-                tsil += 3
-            
-            # Fixed
-            if individual_codon != outgroup_codon:
-
-                # Fixed replacement
-                if get_amino_acid(individual_codon) == get_amino_acid(outgroup_codon):
-                    fr_count += 1
-
-                # Fixed silent
-                elif get_amino_acid(individual_codon) == get_amino_acid(outgroup_codon):
-                    fs_count += 1
-        
-        # If polymorphic
-        elif len(individuals_codons) != 1:
-
-            amino_acids_set = set()
-            for i in individuals_codons:
-                amino_acids_set.add(get_amino_acid(i))
-            
-            # If polymorphic silent
-            if len(amino_acids_set) == 1:
-                ps_count += 1
-            
-            # If polymorphic replacement
-            elif len(amino_acids_set) != 1:
-                pr_count += 1
-        
-    return([pr_count, fr_count, ps_count, fs_count, tsil, trepl])
-
-results = {}
-
-# Iterate over the genes
-with open("snipre_input.csv", "w") as f:
-    # Writes the header
-    f.write("gene_id\tdn\tds\tpn\tps\ttsil\trepl\tnout\tnpop")
-
-    for gene in range(0, columns_number):
-
-        # Create matrix
-        print("Gene: ", data[0][gene].gene_id, "(", gene + 1, "/",columns_number + 1 ,")")
-        current_gene_size = len(data[0][gene].gene_sequence)
-        current_gene_matrix = numpy.empty(shape=[0, current_gene_size])
-
-        for individual in range(0, rows_number):
-            individual_seq_list = [nucleotide for nucleotide in data[individual][gene].gene_sequence]
-            current_gene_matrix = numpy.vstack((current_gene_matrix, individual_seq_list))
-            
-        # Analysis
-        print("Analysis ...")
-        mkr_stats = get_dndspnps(current_gene_matrix, current_gene_size)
-
-        gene_results = result(data[0][gene].gene_id, mkr_stats[0], mkr_stats[1],mkr_stats[2],mkr_stats[3],mkr_stats[4],mkr_stats[5])
-
-        results_line = "\t".join([str(gene_results.gene_id), str(gene_results.pr), str(gene_results.fr), str(gene_results.ps), str(gene_results.fs), str(gene_results.trepl), str(gene_results.tsil), str(nout), str(npop) + "\n"])
-        f.writelines(results_line)
-
-print("Counting done!")       
